24d7bce50d6d5755c1b908f54a58d9bc609ef3e2
Lee, Hyeon-gi <maaya.lee@gmail.com>
2017-12-26 19:07:48 +0900

    Refactoring

--- src/libnodejs/test/TestSuite.js
+++ src/libnodejs/test/TestSuite.js
@@ -1,65 +1,68 @@
+/*jshint esnext: true */
 var TestResult = require('./TestResult');
 
-function TestSuite(suiteName) {
-  this.suiteName = suiteName;
-  this.log = '';
-  this.testCases = [];
-  this.testResult = null;
-  this.currentTestCaseCount = 0;
-  this.currentTestCase = null;
-  this.interval = null;
-}
-
-TestSuite.prototype.add = function(test_case) {
-  this.testCases.push(test_case);
-};
-
-TestSuite.prototype.run = function() {
-  this.testResult = new TestResult(this.suiteName);
-
-  if (this.testCases.length > 0) {
+class TestSuite {
+  constructor(suiteName) {
+    this.suiteName = suiteName;
+    this.log = '';
+    this.testCases = [];
+    this.testResult = null;
     this.currentTestCaseCount = 0;
-    this.currentTestCase = this.testCases[this.currentTestCaseCount];
-    this.currentTestCase.run(this.testResult);
-    var that = this;
-    this.interval = setInterval(function() {
-      that._update();
-    }, 1);
+    this.currentTestCase = null;
+    this.interval = null;
   }
-};
 
-TestSuite.prototype._update = function() {
-  if (this.currentTestCase.isComplete()) {
-    this.currentTestCaseCount++;
-    if (this.currentTestCaseCount === this.testCases.length) {
-      clearInterval(this.interval);
-    }
-    else {
+  add(test_case) {
+    this.testCases.push(test_case);
+  }
+
+  run() {
+    this.testResult = new TestResult(this.suiteName);
+
+    if (this.testCases.length > 0) {
+      this.currentTestCaseCount = 0;
       this.currentTestCase = this.testCases[this.currentTestCaseCount];
       this.currentTestCase.run(this.testResult);
+      var that = this;
+      this.interval = setInterval(function() {
+        that._update();
+      }, 1);
     }
   }
-};
 
-TestSuite.prototype.isComplete = function() {
-  for (var i = 0; i < this.testCases.length; i++) {
-    if (!this.testCases[i].isComplete()) {
-      return false;
+  _update() {
+    if (this.currentTestCase.isComplete()) {
+      this.currentTestCaseCount++;
+      if (this.currentTestCaseCount === this.testCases.length) {
+        clearInterval(this.interval);
+      }
+      else {
+        this.currentTestCase = this.testCases[this.currentTestCaseCount];
+        this.currentTestCase.run(this.testResult);
+      }
     }
   }
-  return true;
-};
 
-TestSuite.prototype.summary= function() {
-  return this.testResult.summary();
-};
+  isComplete() {
+    for (var i = 0; i < this.testCases.length; i++) {
+      if (!this.testCases[i].isComplete()) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-TestSuite.prototype.shortSummary= function() {
-  return this.testResult.shortSummary();
-};
+  summary() {
+    return this.testResult.summary();
+  }
 
-TestSuite.prototype.log = function() {
-  return this.log;
-};
+  shortSummary() {
+    return this.testResult.shortSummary();
+  }
+
+  log() {
+    return this.log;
+  }
+}
 
 module.exports = TestSuite;
